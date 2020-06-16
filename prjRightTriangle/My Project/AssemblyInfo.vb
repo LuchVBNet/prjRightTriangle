4368f902361f1fbeeeda7341474df773ca26caa1
luchmewep <mrstudyfirst@gmail.com>
2020-06-17 06:07:03 +0800

    Added installer at publish folder

--- prjRightTriangle/My Project/AssemblyInfo.vb
+++ prjRightTriangle/My Project/AssemblyInfo.vb
@@ -1,4 +1,5 @@
-﻿Imports System
+﻿Imports System.Resources
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -8,10 +9,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("prjRightTriangle")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("prjRightTriangle")>
+<Assembly: AssemblyTitle("Pythagorean Theorem Calculator")>
+<Assembly: AssemblyDescription("Calculator for getting the hypothenuse of a right triangle.")>
+<Assembly: AssemblyCompany("LuchTech")>
+<Assembly: AssemblyProduct("Pythagorean Theorem Calculator")>
 <Assembly: AssemblyCopyright("Copyright ©  2020")>
 <Assembly: AssemblyTrademark("")>
 
@@ -33,3 +34,4 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en")>
--- prjRightTriangle/My Project/Resources.Designer.vb
+++ prjRightTriangle/My Project/Resources.Designer.vb
@@ -63,16 +63,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property _0114() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("0114", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property _01141() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("01141", resourceCulture)
